eaec719f0e2b673edb0b62fcfa3b2bd5b131f1b8
tclaudel <zekidok@gmail.com>
2020-05-30 11:51:54 +0200

    d04 done

---
+++ bootcamp-python-d04/ex03/HowManyMedals.py
@@ -0,0 +1,20 @@
+from FileLoader import FileLoader
+import pandas as pd
+
+
+def howManyMedals(data, name):
+    filer1 = data["Name"] == name
+    filer2 = data["Medal"]
+    data = data[filer1 & filer2]
+    data = data.set_index("Year")
+    data["M"] = data["Medal"].str[:1]
+    grouped = data.groupby(['Year'])
+    grouped = grouped.M.value_counts()
+    unstacked = grouped.unstack(fill_value=0.0)
+    # print(unstacked.apply(int))
+    return unstacked.to_dict(orient='index')
+
+
+loader = FileLoader()
+data = loader.load("../athlete_events.csv")
+print(howManyMedals(data, 'Kjetil Andr Aamodt'))
